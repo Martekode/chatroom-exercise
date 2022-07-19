285d5b55ee0749839eee5e7e8dca0f1f259a2dd3
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-19 11:05:23 +0200

    added general connectivity and logic for one button

---
+++ client/script.js
@@ -0,0 +1,16 @@
+// defining socket
+let socket = io.connect();
+let messageBox = document.getElementById('inputChat');
+let toAll = document.getElementById('2ALL');
+let toMe = document.getElementById('2ME');
+let target  = document.getElementById('target');
+//button logic
+toAll.addEventListener('click',()=>{
+    socket.emit('sendToAll', (messageBox.value));
+    console.log(messageBox.value);
+});
+
+//receiving 
+socket.on('displayMessage', (message) => {
+    target.innerHTML += '<br>'+message;
+});
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+let counter = 0;
+//requires above
+//defining application
+const app = express();
+//path to our client
+const clientPath = `${__dirname}/../client`;
+//use xpress to host the client
+app.use(express.static(clientPath));
+// use http to serve the app that express provides
+const server = http.createServer(app);
+server.listen(8080, () => {
+    console.log("server running on" + 8080);
+});
+const io = require('socket.io')(server);
+//connection from client to server
+io.on('connection', (socket) => {
+    counter++;
+    console.log(counter + ' someone connected');
+    socket.on('sendToAll', (message) =>{
+        console.log(message);
+        io.emit("displayMessage", (message));
+    });
+});
+
+
+
+
