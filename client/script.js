a51565cb55f49df99a19cab6a99e0acb6bffafe2
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-19 14:24:55 +0200

    made it possible to send to self

--- client/script.js
+++ client/script.js
@@ -6,11 +6,17 @@ let toMe = document.getElementById("2ME");
 let target = document.getElementById("target");
 //button logic
 toAll.addEventListener("click", () => {
-  socket.emit("sendToAll", messageBox.value);
-  console.log(messageBox.value);
+    socket.emit("sendToAll", messageBox.value);
+    console.log(messageBox.value);
+});
+//to me 
+toMe.addEventListener("click", () => {
+    //console.log(hello);
+    socket.emit("sendToMe", messageBox.value);
+    console.log(messageBox.value);
 });
 
 //receiving
 socket.on("displayMessage", (message) => {
-  target.innerHTML += "<br>" + message;
+    target.innerHTML += "<br>" + message;
 });
