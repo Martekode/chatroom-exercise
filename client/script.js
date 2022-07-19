11a78f591cf5628b35a372167c76c485ea1b5018
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-19 12:03:28 +0200

    formatted

--- client/script.js
+++ client/script.js
@@ -1,16 +1,16 @@
 // defining socket
 let socket = io.connect();
-let messageBox = document.getElementById('inputChat');
-let toAll = document.getElementById('2ALL');
-let toMe = document.getElementById('2ME');
-let target  = document.getElementById('target');
+let messageBox = document.getElementById("inputChat");
+let toAll = document.getElementById("2ALL");
+let toMe = document.getElementById("2ME");
+let target = document.getElementById("target");
 //button logic
-toAll.addEventListener('click',()=>{
-    socket.emit('sendToAll', (messageBox.value));
-    console.log(messageBox.value);
+toAll.addEventListener("click", () => {
+  socket.emit("sendToAll", messageBox.value);
+  console.log(messageBox.value);
 });
 
-//receiving 
-socket.on('displayMessage', (message) => {
-    target.innerHTML += '<br>'+message;
+//receiving
+socket.on("displayMessage", (message) => {
+  target.innerHTML += "<br>" + message;
 });
