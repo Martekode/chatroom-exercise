fc6127a78d5a971a3748ceaa0d95475bfbd2503c
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-19 15:52:09 +0200

    added btn logic for initbutton that init the username and displays afterwards the chatroom

--- client/script.js
+++ client/script.js
@@ -5,23 +5,33 @@ let toAll = document.getElementById("2ALL");
 let toMe = document.getElementById("2ME");
 let target = document.getElementById("target");
 let identifier = document.getElementById("identifier");
-console.log(identifier);
+let initButton = document.getElementById("init");
+let chatRoom = document.getElementById("chatroom");
+// console.log(identifier);
+let username;
 let errorMessage = "ok";
 //button logic
-toAll.addEventListener("click", () =>{
-    console.log(identifier);
-    let username = identifier.value;
-    console.log(username);
-    if (!username){
-        console.log("fill in your username!");
+initButton.addEventListener("click", () => {
+    if (!identifier.value){
+        errorMessage = "fill in username";
     }else{
+        username = identifier.value;
+        chatRoom.style.display = "inherit";
+        identifier.style.display = "none";
+        initButton.style.display = "none";
+    }
+});
+//passing data
+toAll.addEventListener("click", () =>{
+    // console.log(identifier);
+    // console.log(username);
         let data = {};
         data.message = messageBox.value;
         data.username = username;
         //console.log(data);
         socket.emit("sendToAll", data);
         //console.log(messageBox.value);
-    }
+    
 });
 //to me 
 toMe.addEventListener("click", () => {
